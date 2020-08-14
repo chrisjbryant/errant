f1ee45221e4d5e30b5a1abefddb9fb30846ce9df
will <xvshiting@live.com>
2020-08-14 16:58:29 +0800

    Add WhitespaceTokenizer
    WhitespaceTokenizer can be used to replace tokenizer.tokens_from_list

--- errant/__init__.py
+++ errant/__init__.py
@@ -1,10 +1,21 @@
 from importlib import import_module
 import spacy
+from spacy.tokens import Doc
 from errant.annotator import Annotator
 
 # ERRANT version
 __version__ = '2.2.1'
 
+class WhitespaceTokenizer(object):
+    def __init__(self, vocab):
+        self.vocab = vocab
+
+    def __call__(self, text):
+        words = text.split(' ')
+        # All tokens 'own' a subsequent space character in this tokenizer
+        spaces = [True] * len(words)
+        return Doc(self.vocab, words=words, spaces=spaces)
+    
 # Load an ERRANT Annotator object for a given language
 def load(lang, nlp=None):
     # Make sure the language is supported
@@ -14,7 +25,7 @@ def load(lang, nlp=None):
 
     # Load spacy
     nlp = nlp or spacy.load(lang, disable=["ner"])
-
+    nlp.tokenizer = WhitespaceTokenizer(nlp.vocab)
     # Load language edit merger
     merger = import_module("errant.%s.merger" % lang)
 
