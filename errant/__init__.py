3ad8f4daff0f14bbebac34af73f5d04e3720f87d
gotuiyan <gto.rikujyo@icloud.com>
2024-03-26 13:18:51 +0900

    __init__.py was returned to its initial state

--- errant/__init__.py
+++ errant/__init__.py
@@ -1,10 +1,28 @@
-from .annotator import Annotator
-from .compare import compare_from_raw, compare_from_edits
-from .edit import Edit
+from importlib import import_module
+import spacy
+from errant.annotator import Annotator
+from errant.compare import compare_from_raw, compare_from_edits
 
 # ERRANT version
 __version__ = '3.0.0'
 
 # Load an ERRANT Annotator object for a given language
 def load(lang, nlp=None):
-    return Annotator.load(lang, nlp)
+        # Make sure the language is supported
+    supported = {"en"}
+    if lang not in supported:
+        raise Exception(f"{lang} is an unsupported or unknown language")
+
+    # Load spacy (small model if no model supplied)
+    nlp = nlp or spacy.load(f"{lang}_core_web_sm", disable=["ner"])
+
+    # Load language edit merger
+    merger = import_module(f"errant.{lang}.merger")
+
+    # Load language edit classifier
+    classifier = import_module(f"errant.{lang}.classifier")
+    # The English classifier needs spacy
+    if lang == "en": classifier.nlp = nlp
+
+    # Return a configured ERRANT annotator
+    return Annotator(lang, nlp, merger, classifier)
